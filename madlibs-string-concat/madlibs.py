049db5a5babfc0a472b8b9da734af97924efdb0b
rbverissimo <rverissimo.dev@gmail.com>
2022-08-13 12:13:51 -0300

    updates in the madlibs mini-game

--- madlibs-string-concat/madlibs.py
+++ madlibs-string-concat/madlibs.py
@@ -2,27 +2,43 @@
 import os
 
 print("Hi! Let's create some insane text \n")
-os.system("pause")
+input("Press any key to continue: \n")
 
-adjective = input("Adjective: ")
-first_verb = input("Verb: ")
-second_verb = input("Another verb: ")
-famous_person = input("A famous person: ")
+def madlib_generator():
+    adjective = input("Adjective: ")
+    first_verb = input("Verb: ")
+    second_verb = input("Another verb: ")
+    famous_person = input("A famous person: ")
 
-if "to" in first_verb.lower():
-    trimmed_first_verb = first_verb.strip()
-    first_verb = trimmed_first_verb.removeprefix("to").strip()
+    if "to" in first_verb.lower():
+        trimmed_first_verb = first_verb.strip()
+        first_verb = trimmed_first_verb.removeprefix("to").strip()
 
-if "to" in second_verb.lower():
-    trimmed_second_verb = second_verb.strip()
-    second_verb = trimmed_second_verb.removeprefix("to").strip()
+    if "to" in second_verb.lower():
+        trimmed_second_verb = second_verb.strip()
+        second_verb = trimmed_second_verb.removeprefix("to").strip()
 
 
-
-madlib = f"Computer program is so {adjective}!! I love to {first_verb} \n " \
+    madlib = f"Computer program is so {adjective}!! I love to {first_verb} \n " \
          f"Stay hidrated and {second_verb}. You are {famous_person}!"
 
-print(madlib)
+    print(madlib)
+    retry()
+
+
+def retry():
+    print("Do you wanna play again? Y or Press any key to finish")
+    retry = input()
+    if retry.lower() == 'y':
+        madlib_generator()
+    else:
+        game_over()
+
+def game_over():
+    print("Thanks for playing!!")
+
+
+madlib_generator()
 
 
 
