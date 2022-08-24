4f1f69bc62d641747ba9785c0aba033850e8ab39
rbverissimo <rverissimo.dev@gmail.com>
2022-08-24 20:20:58 -0300

    updated some string validations

--- hangman/hangman-main.py
+++ hangman/hangman-main.py
@@ -1,6 +1,9 @@
 import random
+import string
+
 from words import words
 
+alphabet = set(string.ascii_uppercase)
 
 def get_valid_word(list_of_words):
     word = random.choice(list_of_words)
@@ -11,13 +14,16 @@ def get_valid_word(list_of_words):
 
 
 def hangman():
-    user_letter = input("Type a letter: ").upper()
+    user_letter = input("Type a letter: ").strip().upper()
+    global alphabet
     used_letters = set()
 
-    while string_validator(user_letter) is False:
+    while string_validator(user_letter) is False or user_letter == '':
         user_letter = letter_selector()
 
-    used_letters.add(user_letter[0])
+    #validation to see which letters have already been used
+    if user_letter in alphabet - used_letters:
+        used_letters.add(user_letter[0])
 
 
 def string_validator(letter):
@@ -39,7 +45,8 @@ def selection_validator():
 
 
 def letter_selector():
-    user_letter = input("Type a letter: ").upper()
+    user_letter = input("Type a letter: ").strip().upper()
     return user_letter
+
 
 hangman()
