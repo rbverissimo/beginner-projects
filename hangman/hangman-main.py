751c6549e84d88aa1b94b6a1f831c1d2a76ca5ff
rbverissimo <rverissimo.dev@gmail.com>
2022-08-25 09:54:59 -0300

    modifying words.py to list/updating game logic in hangman.py

--- hangman/hangman-main.py
+++ hangman/hangman-main.py
@@ -1,19 +1,21 @@
 import random
 import string
-
 from words import words
 
 alphabet = set(string.ascii_uppercase)
 
-def get_valid_word(list_of_words):
-    word = random.choice(list_of_words)
+
+def get_valid_word(words):
+    word = random.choice(words)
     while '-' in word or ' ' in word:
-        word = random.choice(list_of_words)
+        word = random.choice(words)
 
     return word
 
 
 def hangman():
+    word = get_valid_word(words)
+    word_letters = set(word)
     user_letter = input("Type a letter: ").strip().upper()
     global alphabet
     used_letters = set()
@@ -21,9 +23,11 @@ def hangman():
     while string_validator(user_letter) is False or user_letter == '':
         user_letter = letter_selector()
 
-    #validation to see which letters have already been used
+    # validation to see which letters have already been used
     if user_letter in alphabet - used_letters:
         used_letters.add(user_letter[0])
+        if user_letter[0] in word_letters:
+            word_letters.remove(user_letter)
 
 
 def string_validator(letter):
