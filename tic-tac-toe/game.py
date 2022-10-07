6323fe7c7219978ed09438b3c27d00fe0802a1b8
rbverissimo <rverissimo.dev@gmail.com>
2022-10-07 12:27:22 -0300

    winner logic implemented

--- tic-tac-toe/game.py
+++ tic-tac-toe/game.py
@@ -39,8 +39,32 @@ class TicTacToe:
             return True
         return False
 
+    def winner(self, square, letter):
+        # check the row, square is the index in the list
+        row_ind = square // 3
+        row = self.board[row_ind*3: (row_ind + 1) * 3]
+        if all([spot == letter for spot in row]):
+            return True
+
+        # check column
+        col_ind = square % 3 # division leftovers
+        column = [self.board[col_ind+i*3] for i in range(3)]
+        if all([spot == letter for spot in column]):
+            return True
+
+        # check diagonal: 0, 2, 4, 6, 8
+        if square % 2 == 0:
+            diagonal1 = [self.board[i] for i in [0, 4, 8]]
+            if all([spot == letter for spot in diagonal1]):
+                return True
+            diagonal2 = [self.board[i] for i in [2, 4, 6]]
+            if all([spot == letter for spot in diagonal2]):
+                return True
+        return False
+
 
 def play(game, x_player, o_player, print_game=True):
+    # the letter indicates the winner, none indicates a tie
     if print_game:
         game.print_board_nums()
 
@@ -60,5 +84,9 @@ def play(game, x_player, o_player, print_game=True):
             if game.current_winner:
                 if print_game:
                     print(letter + ' wins!!')
-                    
+                return letter
+
             letter = 'O' if letter == 'X' else 'X'  # this mean to switch players
+
+            if print_game:
+                print('It\'s a tie!')
