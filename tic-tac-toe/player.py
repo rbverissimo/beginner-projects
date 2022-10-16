922c3eb9c07d62f9251e5978a806d96b0e3c0fa2
rbverissimo <rverissimo.dev@gmail.com>
2022-10-16 16:32:33 -0300

    implementing minimax logic, defining dictionaries

--- tic-tac-toe/player.py
+++ tic-tac-toe/player.py
@@ -56,3 +56,23 @@ class GeniusComputerPlayer(Player):
     def minimax(self, state, player):
         max_player = self.letter
         other_player = 'O' if player == 'X' else 'X'
+
+        if state.current_winner == other_player:
+            return {'position': None,
+                    'score': 1 * (state.num_empty_squares() + 1) if other_player == max_player else -1 * (
+                        state.num_empty_squares() + 1)
+                    }
+        elif not state.empty_squares():
+            return {'position': None, 'score': 0}
+
+        if player == max_player:
+            best = {'position': None, 'score': -math.inf}  # initializing it to the lowest possible
+        else:
+            best = {'position': None, 'score': math.inf}  # better initialize it to the maximum possible
+
+        for possible_move in state.available_moves():
+            # 1: make a move, try that spot
+            state.make_move(possible_move, player)
+            # 2: recurse minimax to simulate a game after that move, hence the loop
+            # 3: undo the move, so that it can test other moves
+            # 4: update the dictionaries
