a8e0093d58d5298428a345bb2bc9cfbe91cbf0b3
rbverissimo <rverissimo.dev@gmail.com>
2022-10-19 00:03:10 -0300

    setting bombs implementation based on the 2D board implementation

--- minesweeper/minesweeper.py
+++ minesweeper/minesweeper.py
@@ -1,3 +1,4 @@
+import random
 
 
 class Board:
@@ -10,8 +11,14 @@ class Board:
         self.dug = set()  # save tuples (0,0) to where the player have opened
 
     def make_new_board(self):
-        # list of lists 2D board: 
+        # list of lists 2D board:
         board = [[None for _ in range(self.dim_size)] for _ in range(self.dim_size)]
+        # the board will be set as a bunch of nones instead of fields
+        bombs_set = 0
+        while bombs_set < self.num_bombs:
+            loc = random.randint(0, self.dim_size**2 - 1)  # since the board is 10x10 for example, 99 choices
+            row = loc // self.dim_size
+            col = loc % self.dim_size
 
 
 def play(dim_size=10, num_bombs=10):
